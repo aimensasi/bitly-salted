50a775f216f8538bf73ee373b9a673eea28dffa1
aimensasi <ali.sasi@ymail.com>
2016-11-09 17:57:25 +0800

    validating URL
    going back to basic validation using regex, because using other methods
    causes a lot off errors

--- app/models/url.rb
+++ app/models/url.rb
@@ -3,22 +3,13 @@ class Url < ActiveRecord::Base
 	REGEX_URL = /^(https|http):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
 	
 	before_create do 
-		is_valid
 		self.short_form = shorten
 	end
 	
-	validates :url, :presence => true, :uniqueness => true
+	validates :url, :presence => true, :uniqueness => true, :format => {:with => REGEX_URL, :multiline => true}
 
 	def shorten
 		letters = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map { |i| i.to_a }.flatten
 		string = (0...5).map { letters[rand(letters.length)] }.join
-	end
-
-	
-	private
-	def is_valid
-		if self.url.match(REGEX_URL)
-			self.url = "https://#{self.url}"
-		end
 	end
 end
