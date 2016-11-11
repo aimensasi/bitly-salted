3151816d0a918f3f5fea529e887561d7c49da5c8
aimensasi <ali.sasi@ymail.com>
2016-11-11 15:24:22 +0800

    Seeding the database with 500,000 urls

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,20 @@ class ImportUrls
 		urls = []
 		i = 0
 		CSV.foreach(FILE_NAME) do |csv_row|
-			url_row = csv_row[0].insert(csv_row[0].index(')'), ", #{Url.shorten}")
-			urls.push(url_row)
+			# puts csv_row
+			url_row = csv_row.first.gsub(/[(]/, "").gsub(/[)]/, "")
+			url_row = "'#{url_row}'"
+			# puts url_row
+			url_row = url_row.insert(csv_row.first.length , ", '#{Url.shorten}', LOCALTIMESTAMP, LOCALTIMESTAMP")
+
+			# puts "(#{url_row})"								 
+			urls.push("(#{url_row})")
+			# return
 		end
 
+
 		Url.transaction do
-			Url.connection.execute "INSERT INTO urls (url, short_form) VALUES #{urls.join(', ')}"
+			Url.connection.execute "INSERT INTO urls (url, short_form, created_at, updated_at) VALUES #{urls.join(', ')}"
 		end
 	end
 
